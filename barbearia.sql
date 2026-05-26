25c47418211a3e0f40d92ab5afead61377368f2a
leandsonpy <leandsoncarvalho07@gmail.com>
2026-05-26 09:23:35 -0300

    Add files via upload

---
+++ barbearia.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS barbearia;
+USE barbearia;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS agendamentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_nome VARCHAR(100) NOT NULL,
+    data_agendada DATE NOT NULL,
+    horario TIME NOT NULL
+);
